3352e8b92bd985b7acee474f52360aea142c0e3f
Rich Harris <richard.a.harris@gmail.com>
2015-07-16 15:35:35 -0400

    more stuff

--- gobblefile.js
+++ gobblefile.js
@@ -3,13 +3,22 @@ var gobble = require( 'gobble' );
 module.exports = gobble([
 
 	gobble( 'src/files' ),
+	gobble( 'node_modules/acorn/dist/acorn.js' ),
 
 	gobble( 'src/app' )
 		.transform( 'ractive', { type: 'es6' })
 		.transform( 'rollup-babel', {
 			entry: 'index.js',
 			format: 'cjs',
-			external: [ 'ractive', 'eases' ]
+			external: [
+				'ractive',
+				'eases',
+				'jshint',
+				'codemirror',
+				'codemirror/keymap/sublime',
+				'codemirror/addon/search/searchcursor',
+				'codemirror/mode/javascript/javascript'
+			]
 		})
 		.transform( 'derequire' )
 		.transform( 'browserify', {
